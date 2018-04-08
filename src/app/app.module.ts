fd24e2cd3097ca82ab07cd1fc7680238974c0e6b
jose <jose@localhost.localdomain>
2018-04-08 14:58:58 +0800

    You refactored data access to the HeroService class. You provided the HeroService in the root AppModule so that it can be injected anywhere. You used Angular Dependency Injection to inject it into a component. You gave the HeroService get data method an asynchronous signature. You discovered Observable and the RxJS Observable library. You used RxJS of() to return an Observable of mock heroes (Observable<Hero[]>). The component's ngOnInit lifecycle hook calls the HeroService method, not the constructor. You created a MessageService for loosely-coupled communication between classes. The HeroService injected into a component is created with another injected service, MessageService.
    	modified:   src/app/app.component.html
    	modified:   src/app/app.module.ts
    	new file:   src/app/hero.service.spec.ts
    	new file:   src/app/hero.service.ts
    	modified:   src/app/heroes/heroes.component.ts
    	new file:   src/app/message.service.spec.ts
    	new file:   src/app/message.service.ts
    	new file:   src/app/messages/messages.component.css
    	new file:   src/app/messages/messages.component.html
    	new file:   src/app/messages/messages.component.spec.ts
    	new file:   src/app/messages/messages.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,23 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
+import { HeroService } from './hero.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule  
   ],
-  providers: [],
+  providers: [HeroService, MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
